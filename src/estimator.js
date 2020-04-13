5c5e1905969775bd07f497d60baf2ea795d2b53d
chidiHenry <emeribe@gmail.com>
2020-04-14 00:32:17 +0100

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,3 @@
-/* eslint-disable linebreak-style */
-
 
 const covid19ImpactEstimator = (data) => ({
   return: {
@@ -17,28 +15,27 @@ const covid19ImpactEstimator = (data) => ({
       totalHospitalBeds: 1380614
     },
     impact: {
-      currentlyInfected: Math.trunc(data.reportedCases * 10),
-      infectionsByRequestedTime: Math.trunc(this.currentlyInfected * 2 ** 9),
-      severeCasesByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.15),
-      totalHospitalBeds: Math.trunc(this.severeCasesByRequestedTime * 0.35),
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: this.currentlyInfected * 2 ** 9,
+      severeCasesByRequestedTime: this.infectionsByRequestedTime * 0.15,
+      totalHospitalBeds: this.severeCasesByRequestedTime * 0.35,
       hospitalBedsByRequestedTime:
-      Math.trunc(this.severeCasesByRequestedTime / this.totalHospitalBeds),
-      casesForICUByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.05),
-      casesForVentilatorsByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.02),
+      this.severeCasesByRequestedTime / this.totalHospitalBeds,
+      casesForICUByRequestedTime: this.infectionsByRequestedTime * 0.05,
+      casesForVentilatorsByRequestedTime: this.infectionsByRequestedTime * 0.02,
       dollarsInFlight:
-      Math.trunc(this.infectionsByRequestedTime * data.avgDailyIncomePopulation * data.periodType)
+      this.infectionsByRequestedTime * data.avgDailyIncomePopulation * data.periodType
     },
     severeImpact: {
-      currentlyInfected: Math.trunc(data.reportedCases * 50),
-      infectionsByRequestedTime: Math.trunc(this.currentlyInfected * 2 ** 9),
-      severeCasesByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.15),
-      totalHospitalBeds: Math.trunc(this.severeCasesByRequestedTime * 0.35),
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: this.currentlyInfected * 2 ** 9,
+      severeCasesByRequestedTime: this.infectionsByRequestedTime * 0.15,
+      totalHospitalBeds: this.severeCasesByRequestedTime * 0.35,
       hospitalBedsByRequestedTime:
-      Math.trunc(this.severeCasesByRequestedTime / this.totalHospitalBeds),
-      casesForICUByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.05),
-      casesForVentilatorsByRequestedTime: Math.trunc(this.infectionsByRequestedTime * 0.02),
-      dollarsInFlight:
-      Math.trunc(this.infectionsByRequestedTime * data.avgDailyIncomePopulation * data.periodType)
+      this.severeCasesByRequestedTime / this.totalHospitalBeds,
+      casesForICUByRequestedTime: this.infectionsByRequestedTime * 0.05,
+      casesForVentilatorsByRequestedTime: this.infectionsByRequestedTime * 0.02,
+      dollarsInFlight: this.infectionsByRequestedTime * data.avgDailyIncomePopulation * data.periodType
 
     }
   }
